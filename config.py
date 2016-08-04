1d717c72dbc8ed6439607123eb671d75e31d08e9
Rick <jiangchuanwei2012@163.com>
2016-08-04 15:08:00 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 
-sql_server_host='192.168.131.156'
+sql_server_host=''
 sql_server_port=3306
-sql_server_username='root'
-sql_server_password='sdust'
-sql_server_db_name='spider'
+sql_server_username=''
+sql_server_password=''
+sql_server_db_name=''
 sql_server_db_charset='utf-8'
 
 start_urls=['https://www.juhe.cn/docs']
